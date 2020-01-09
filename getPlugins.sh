a30cfca2c3e67e15beea2659a80e77ba4afacd9b
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-01-09 12:49:44 -0500

    Initial commit

---
+++ getPlugins.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/movingpictures83/Extract.git ../../plugins/Extract
+git clone https://github.com/movingpictures83/ProcessDesc.git ../../plugins/ProcessDesc
+git clone https://github.com/movingpictures83/Amplicon.git ../../plugins/Amplicon
+
